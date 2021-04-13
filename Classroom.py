96b6a3ca2a6087dd5bae19e0cff8aafcedef2e44
Saptak Das <saptak.das625@gmail.com>
2021-04-12 21:29:48 -0400

    Excel Helper Function

--- Classroom.py
+++ Classroom.py
@@ -1,11 +1,8 @@
 from dbFunctions import addClassroom, queryByName, queryForEntries
 import sys
 import os
-from helperFunctions import generateQRCode
+from helperFunctions import generateQRCode, generateExcelSheet
 from zipfile import ZipFile
-from openpyxl import Workbook
-from openpyxl.styles import Font, Alignment
-import datetime
 
 class Classroom:
     def __init__(self, name, numOfSeats, owner, roomId=None, from_database = False):
@@ -58,53 +55,5 @@ class Classroom:
         #Get entries for Classroom
         #DO DATABASE SEARCH TO GET ALL CLASSROOMS HERE.
         results = queryForEntries(self.name)
-        
-        #Add Extra Empty Records
-        valuesWithRecords = {i[3]: None for i in results}
-        for desk in range(self.numOfSeats):
-            if desk+1 not in valuesWithRecords:
-                results.append(['', '', self.name, desk+1, ''])
-
-        #Sort Results by desks
-        def sortByDeskNumber(e):
-          return e[3]
-        results.sort(key=sortByDeskNumber)  
-
-        #Create New Excel File
-        workbook = Workbook()
-        sheet = workbook.active
-
-        #Make Header
-        headerTitles = ['Desk', 'Name', 'Email', 'Time']
-        ordOfA = ord('A')
-        for i, header in enumerate(headerTitles):
-          sheet[f'{chr(ordOfA+i)}1'] = header
-          sheet[f'{ chr(ordOfA+i) }1'].font = Font(bold=True)
-
-        #Make entries one by one
-        deskNumbers = {}
-        lastNumber = None
-        for i, entry in enumerate(results):
-            sheet[f'A{i+2}'] = entry[3]
-            sheet[f'A{i+2}'].alignment = Alignment(horizontal='center', vertical='center')
-            sheet[f'B{i+2}'] = entry[0]
-            sheet[f'C{i+2}'] = entry[1]
-            sheet[f'D{i+2}'] = entry[4]
-            if lastNumber != entry[3]:
-                if lastNumber != None:
-                    deskNumbers[lastNumber].append(i+2-1)
-                lastNumber = entry[3]
-                deskNumbers[lastNumber] = [i+2]
-        if lastNumber != None:
-            deskNumbers[lastNumber].append(len(results)-1+2)
-        #Merge Desk Number
-        for key in deskNumbers:
-          value = deskNumbers[key]
-          if value[0] != value[1]:
-            #Merge cells
-            sheet.merge_cells(f'A{value[0]}:A{value[1]}')
-            sheet[f'A{value[0]}']
-
-        #Save File once edits are made
-        workbook.save(filename=f"{self.name}_{str(datetime.datetime.now()).split(' ')[0]}.xlsx")
-        return f"{self.name}_{str(datetime.datetime.now()).split(' ')[0]}.xlsx"
+        filename = generateExcelSheet(results, self.name, self.numOfSeats)
+        return filename
